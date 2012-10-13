8d42c83e561e06193bcb5e53eec663063de763d3
David Haslem <therabidbanana@gmail.com>
2012-10-12 22:16:29 -0400

    Make intersection a class

--- app/models/intersection.rb
+++ app/models/intersection.rb
@@ -1,29 +1,34 @@
-users = Hash.new
+class Intersection
+  def demo_method
+    users = Hash.new
 
-LENGTH       = 12
-MIN_DURATION = 3
+    LENGTH       = 12
+    MIN_DURATION = 3
 
-users["adam"]  = "1111110111110"
-users["jason"] = "1111111100010"
-users["david"] = "1100111111110"
+    users["adam"]  = "1111110111110"
+    users["jason"] = "1111111100010"
+    users["david"] = "1100111111110"
 
-users.size.downto(2) do |n|
-  found = false
+    users.size.downto(2) do |n|
+      found = false
 
-  users.keys.combination(n).to_a.each do |subgroup|
-    intersection = subgroup.inject(("1" * LENGTH).to_i(2)) do |x, user|
-      x &= users[user].to_i(2)
-    end.to_s(2)
+      users.keys.combination(n).to_a.each do |subgroup|
+        intersection = subgroup.inject(("1" * LENGTH).to_i(2)) do |x, user|
+          x &= users[user].to_i(2)
+        end.to_s(2)
 
-    intersection.gsub!(/(?:^|0)(1{1,#{MIN_DURATION-1}})(?:0|$)/, "0" * '\1'.length)
+        intersection.gsub!(/(?:^|0)(1{1,#{MIN_DURATION-1}})(?:0|$)/, "0" * '\1'.length)
 
-    if intersection =~ /1{#{MIN_DURATION}}/
-      found = true
+        if intersection =~ /1{#{MIN_DURATION}}/
+          found = true
 
-      puts subgroup.sort.inspect
-      puts intersection.rjust(LENGTH, "0")
+          puts subgroup.sort.inspect
+          puts intersection.rjust(LENGTH, "0")
+        end
+      end
+
+      break if found
     end
+    found
   end
-
-  exit if found
 end
