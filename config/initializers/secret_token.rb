cf71e7cbff3e4f4870dcaaa6ba77ea5f21cd46bc
David Haslem <therabidbanana@gmail.com>
2012-10-12 20:08:55 -0400

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DtimeRumble::Application.config.secret_token = '352032c729524f02646fe068857aa75326bd220682d8f744e9a099ac164a35dbf640686a806c24e29a228e12f3a15e8207ee701572cd6df897a0b1cc9dc79668'
